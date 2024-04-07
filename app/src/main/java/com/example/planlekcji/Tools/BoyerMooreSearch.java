f5f6454c7fdf73d131c74345cb64e90409f6e758
Bokeher <bokeher47@gmail.com>
2024-04-07 20:07:28 +0200

    Fixed BoyerMooreSearch algorithm.

--- app/src/main/java/com/example/planlekcji/Tools/BoyerMooreSearch.java
+++ app/src/main/java/com/example/planlekcji/Tools/BoyerMooreSearch.java
@@ -3,30 +3,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BoyerMooreSearch {
-    private static void preProcessPattern(char[] pattern, Map<Character, Integer> shift) {
-        int m = pattern.length;
-        for (int i = 0; i < m; i++) {
-            shift.put(pattern[i], m - i - 1);
+    private Map<Character, Integer> lastOccurrence;
+
+    public BoyerMooreSearch() {
+        lastOccurrence = new HashMap<>();
+    }
+
+    private void preprocessPattern(String pattern) {
+        lastOccurrence.clear();
+        for (int i = 0; i < pattern.length(); i++) {
+            lastOccurrence.put(pattern.charAt(i), i);
         }
     }
 
-    public static boolean search(String text, String pattern) {
+    public boolean search(String text, String pattern) {
         int n = text.length();
         int m = pattern.length();
-        Map<Character, Integer> shift = new HashMap<>();
-        preProcessPattern(pattern.toCharArray(), shift);
-        int s = 0;
-        while (s <= n - m) {
-            int j = m - 1;
-            while (j >= 0 && pattern.charAt(j) == text.charAt(s + j))
-                j--;
-            if (j < 0) {
-                return true;
-            } else {
-                char mismatchedChar = text.charAt(s + j);
-                Integer charShift = shift.getOrDefault(mismatchedChar, m);
-                s += Math.max(1, j - charShift);
+        preprocessPattern(pattern);
+
+        int skip;
+        for (int i = 0; i <= n - m; i += skip) {
+            skip = 0;
+            for (int j = m - 1; j >= 0; j--) {
+                if (pattern.charAt(j) != text.charAt(i + j)) {
+                    skip = Math.max(1, j - lastOccurrence.getOrDefault(text.charAt(i + j), -1));
+                    break;
+                }
             }
+            if (skip == 0) return true;
         }
         return false;
     }

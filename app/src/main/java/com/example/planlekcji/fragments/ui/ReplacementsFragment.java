53518a556e70f36025844121fcd3ce7cfdf15712
Bokeher <bokeher47@gmail.com>
2025-01-17 20:40:05 +0100

    Searching code cleanup

--- app/src/main/java/com/example/planlekcji/fragments/ui/ReplacementsFragment.java
+++ app/src/main/java/com/example/planlekcji/fragments/ui/ReplacementsFragment.java
@@ -1,20 +1,17 @@
 package com.example.planlekcji.fragments.ui;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.text.Html;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.example.planlekcji.MainActivity;
 import com.example.planlekcji.MainViewModel;
 import com.example.planlekcji.R;
 import com.example.planlekcji.utils.BoyerMooreSearch;
@@ -31,7 +28,7 @@ public class ReplacementsFragment extends Fragment {
 
     // used for searching
     private List<String> replacementsWithoutHtml;
-    private HashSet<Integer> replacementIds;
+    private HashSet<Integer> replacementIdsToShow; // Ids of replacements that are supposed to be shown after being filtered by searching
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -50,27 +47,25 @@ public class ReplacementsFragment extends Fragment {
         mainViewModel.getReplacementsLiveData().observe(getViewLifecycleOwner(), newReplacements -> {
             replacements = newReplacements;
 
-            // Prepare replacements without html for searching
+            // Prepare replacements without html for searching (also remove multiple spaces caused by removing html tags)
             replacementsWithoutHtml = replacements.stream()
                     .map(s -> s.replaceAll("<[^>]*>", "").replaceAll("\\s+", " "))
                     .collect(Collectors.toList());
 
-            setReplacements();
+            updateReplacements();
         });
     }
 
     private void setEventListenerToSearchBar() {
         EditText searchBar = view.findViewById(R.id.editText_searchBar);
 
-
         searchBar.addTextChangedListener(new DelayedSearchTextWatcher(query -> {
-            replacementIds = searchReplacements(query.toLowerCase());
-
-            setReplacements();
+            replacementIdsToShow = searchReplacements(query.toLowerCase());
+            updateReplacements();
         }));
     }
 
-    private void setReplacements() {
+    private void updateReplacements() {
         TextView textFieldReplacements = view.findViewById(R.id.textView_replacements);
         EditText searchBar = view.findViewById(R.id.editText_searchBar);
         View divider = view.findViewById(R.id.divider);
@@ -81,59 +76,38 @@ public class ReplacementsFragment extends Fragment {
             divider.setVisibility(View.GONE);
 
             textFieldReplacements.setText(getString(R.string.no_replacements));
-        } else {
-            searchBar.setVisibility(View.VISIBLE);
-            divider.setVisibility(View.VISIBLE);
-
-            if (replacementIds == null || replacementIds.isEmpty()) {
-                textView_noResults.setVisibility(replacementIds == null ? View.GONE : View.VISIBLE);
-                textFieldReplacements.setText(Html.fromHtml(String.join("<br><br>", replacements), Html.FROM_HTML_MODE_LEGACY));
-                return;
-            }
-
-            List<String> filteredReplacements = replacements.stream()
-                    .filter(rep -> replacementIds.contains(replacements.indexOf(rep)))
-                    .collect(Collectors.toList());
-
-            textFieldReplacements.setText(Html.fromHtml(String.join("<br><br>", filteredReplacements), Html.FROM_HTML_MODE_LEGACY));
-            textView_noResults.setVisibility(View.GONE);
-
+            return;
         }
-    }
 
-    private void setReplacements(String data) {
-        if (data == null) return;
+        searchBar.setVisibility(View.VISIBLE);
+        divider.setVisibility(View.VISIBLE);
 
-        TextView textFieldReplacements = view.findViewById(R.id.textView_replacements);
-        textFieldReplacements.setText(Html.fromHtml(data, Html.FROM_HTML_MODE_LEGACY));
+        if (replacementIdsToShow == null || replacementIdsToShow.isEmpty()) {
+            textView_noResults.setVisibility(replacementIdsToShow == null ? View.GONE : View.VISIBLE);
+            textFieldReplacements.setText(Html.fromHtml(String.join("<br><br>", replacements), Html.FROM_HTML_MODE_LEGACY));
+            return;
+        }
+
+        List<String> filteredReplacements = replacements.stream()
+                .filter(rep -> replacementIdsToShow.contains(replacements.indexOf(rep)))
+                .collect(Collectors.toList());
+
+        textFieldReplacements.setText(Html.fromHtml(String.join("<br><br>", filteredReplacements), Html.FROM_HTML_MODE_LEGACY));
+        textView_noResults.setVisibility(View.GONE);
     }
 
     private HashSet<Integer> searchReplacements(String searchingKey) {
         if (searchingKey.isEmpty()) return null;
 
         BoyerMooreSearch boyerMooreSearch = new BoyerMooreSearch();
-
-        // searching
         HashSet<Integer> replacementIds = new HashSet<>();
-        for (int i = 0; i < replacementsWithoutHtml.size(); i++) {
-            String replacement = replacementsWithoutHtml.get(i);
 
-            if (boyerMooreSearch.search(replacement.toLowerCase(), searchingKey)) {
+        for (int i = 0; i < replacementsWithoutHtml.size(); i++) {
+            if (boyerMooreSearch.search(replacementsWithoutHtml.get(i).toLowerCase(), searchingKey)) {
                 replacementIds.add(i);
             }
-
         }
 
         return replacementIds;
-    }
-
-    private String updateSearchBar() {
-        SharedPreferences sharedPref = MainActivity.getContext().getSharedPreferences("sharedPrefs", 0);
-        String searchKey = sharedPref.getString(getString(R.string.searchKey), "");
-
-        EditText searchBar = view.findViewById(R.id.editText_searchBar);
-        searchBar.setText(searchKey);
-
-        return searchKey;
     }
 }

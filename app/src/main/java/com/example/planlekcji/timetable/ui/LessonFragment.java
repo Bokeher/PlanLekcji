2b1e8b323752766ea18f0cb98a68a9091c960c99
Bokeher <bokeher47@gmail.com>
2025-01-18 20:06:49 +0100

    Fix some warnings

--- app/src/main/java/com/example/planlekcji/timetable/ui/LessonFragment.java
+++ app/src/main/java/com/example/planlekcji/timetable/ui/LessonFragment.java
@@ -33,7 +33,7 @@ import java.util.Map;
 import java.util.Objects;
 
 public class LessonFragment extends Fragment {
-    List<String> lessonHours = new ArrayList<>();
+    private final List<String> lessonHours = new ArrayList<>();
 
     public static final String TITLE = "title";
     private Map<DayOfWeek, List<String>> timetableMap;
--- app/src/main/java/com/example/planlekcji/utils/ToastUtils.java
+++ app/src/main/java/com/example/planlekcji/utils/ToastUtils.java
@@ -6,9 +6,8 @@ import android.widget.Toast;
 
 public class ToastUtils {
     public static void showToast(final Context context, final String message, boolean closeApplication) {
-        if (!(context instanceof Activity)) { return; }
+        if (!(context instanceof Activity activity)) { return; }
 
-        final Activity activity = (Activity) context;
         activity.runOnUiThread(() -> {
             Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
             if(closeApplication) activity.finish();

438e00679ecb15adde120204bd44986142e2612c
timsixth <timsixth@op.pl>
2026-09-11 14:37:33 +0200

    Changed tests to old version

--- app/src/test/java/com/example/planlekcji/ckziu_elektryk/client/ReplacementServiceTest.java
+++ app/src/test/java/com/example/planlekcji/ckziu_elektryk/client/ReplacementServiceTest.java
@@ -1,38 +1,29 @@
 package com.example.planlekcji.ckziu_elektryk.client;
 
-
 import static org.junit.Assert.assertNotNull;
 
 import com.example.planlekcji.ckziu_elektryk.client.replacements.Replacement;
 import com.example.planlekcji.ckziu_elektryk.client.replacements.ReplacementRequest;
 import com.example.planlekcji.ckziu_elektryk.client.replacements.ReplacementService;
 import com.example.planlekcji.ckziu_elektryk.client.replacements.ReplacementType;
-import com.example.planlekcji.ckziu_elektryk.client.stubs.CKZiUElektrykClientStub;
-import com.example.planlekcji.ckziu_elektryk.client.stubs.ConfigStub;
-import com.example.planlekcji.ckziu_elektryk.client.stubs.TestConstants;
+import com.example.planlekcji.ckziu_elektryk.client.stubs.CKZiUElektrykClientStubFactory;
 import com.example.planlekcji.ckziu_elektryk.client.utils.DateUtil;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-
 public class ReplacementServiceTest {
 
     private ReplacementService replacementService;
-    private Config config;
 
     @Before
     public void init() throws IOException {
-        config = new ConfigStub();
-        CKZiUElektrykClient client = new CKZiUElektrykClientStub(config);
+        CKZiUElektrykClient client = CKZiUElektrykClientStubFactory.createClient();
 
         replacementService = client.getReplacementService();
     }
@@ -62,18 +53,6 @@ public class ReplacementServiceTest {
     public void shouldGetReplacementsByPeriod() {
         Date startDate = DateUtil.parseDate(ReplacementRequest.REPLACEMENT_DATE_PATTERN, "2026-09-07");
         Date endDate = DateUtil.parseDate(ReplacementRequest.REPLACEMENT_DATE_PATTERN, "2026-09-11");
-
-        config = Mockito.mock(Config.class);
-
-        try(MockWebServer server = new MockWebServer()) {
-            Mockito.when(config.getAPIUrl()).thenReturn(server.url("/").url().toString());
-            Mockito.when(config.getToken()).thenReturn("token");
-            server.enqueue(new MockResponse().
-                    setResponseCode(200)
-                    .setBody(TestConstants.RESPONSE_REPLACEMENTS_PERIOD));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
 
         Map<Date, List<Replacement>> replacements = replacementService.getReplacements(ReplacementType.TEACHERS, startDate, endDate);
 
